09a8ed240fce7cffedcc9199a52bfc1d8c943e2c
Kennawang6 <Kenna.wang6@gmail.com>
2018-02-21 12:45:40 -0800

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -1,1 +1,37 @@
+#include "StudentWorld.h"
+#include "GameConstants.h"
+#include <string>
+#include "Actor.h"
+#include <list>
+using namespace std;
 
+GameWorld* createStudentWorld(string assetDir)
+{
+	return new StudentWorld(assetDir);
+}
+
+// Students:  Add code to this file, StudentWorld.h, Actor.h and Actor.cpp
+
+StudentWorld::StudentWorld(string assetDir)
+: GameWorld(assetDir){
+    //initialize new data members?
+}
+
+int StudentWorld::init()
+{
+    m_player = new NachenBlaster(this);
+    for (int i = 0; i < 30; i++)
+        m_actors.push_back(new Star(this, randInt(0, 255)));
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+int StudentWorld::move(){
+    // This code is here merely to allow the game to build, run, and terminate after you hit enter.
+    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+    decLives();
+    return GWSTATUS_PLAYER_DIED;
+}
+
+void StudentWorld::cleanUp(){
+    
+}
