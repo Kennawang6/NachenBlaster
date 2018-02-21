f846f624346b1c7eec684a2d7e9cc18edbbd71b5
Kennawang6 <Kenna.wang6@gmail.com>
2018-02-21 12:46:04 -0800

    Update StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -1,1 +1,25 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
 
+#include "GameWorld.h"
+#include <string>
+#include <list>
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class Actor;
+class NachenBlaster;
+
+class StudentWorld : public GameWorld
+{
+public:
+    StudentWorld(std::string assetDir);
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+private:
+    std::list<Actor*> m_actors;
+    NachenBlaster* m_player;
+};
+
+#endif // STUDENTWORLD_H_
