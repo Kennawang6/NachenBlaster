23053bb72d640da12d2fe9488ba9656aaa0fe405
Kennawang6 <Kenna.wang6@gmail.com>
2018-02-28 02:14:38 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -10,21 +10,60 @@ Actor::Actor(StudentWorld* world, int imageID, double startX, double startY, int
 
 }
 
+Actor::~Actor(){}
+
 StudentWorld* Actor::getWorld(){
     return m_world;
 }
 
 
 
-/////////////////
-//NachenBlaster//
-/////////////////
-NachenBlaster::NachenBlaster(StudentWorld* world): Actor(world, IID_NACHENBLASTER, 0, 128), m_cabbages(30), m_torpedoes(0), m_health(50){
+////////
+//Ship//
+////////
+Ship::Ship(StudentWorld* world, int imageID, int health, double startX, double startY, int dir, double size, int depth): Actor(world, imageID, startX, startY, dir, size, depth), m_health(health){
+
+}
+
+Ship::~Ship(){}
+
+int Ship::getHealth(){
+    return m_health * 2;
+}
+
+
+
+/////////
+//Alien//
+/////////
+Alien::Alien(StudentWorld* world, int imageID, int health, double startX, double startY, int dir, double speed): Ship(world, imageID, health, startX, startY, dir, 1.5, 1), m_travelSpeed(speed){
     
 }
 
+Alien::~Alien(){
+    getWorld()->killedAlien();
+}
+
+
+
+////////////
+//Smallgon//
+////////////
+//Smallgon::Smallgon(StudentWorld* world): Alien(world, IID_SMALLGON, 5 * (1 + (world->getLevel() – 1) * .1), x, y)
+
+
+
+/////////////////
+//NachenBlaster//
+/////////////////
+NachenBlaster::NachenBlaster(StudentWorld* world): Ship(world, IID_NACHENBLASTER, 50, 0, 128), m_cabbages(30), m_torpedoes(10){
+    
+}
+
+NachenBlaster::~NachenBlaster(){}
+
 bool NachenBlaster::isAlive(){
-    if (m_health < 0)
+    if (getHealth() < 0)
         return false;
     else
         return true;
@@ -34,6 +73,7 @@ void NachenBlaster::doSomething(){
     if (!isAlive())
         return;
     int ch;
+    
     if(getWorld()->getKey(ch)){
         switch (ch){
             case KEY_PRESS_LEFT:
@@ -52,12 +92,25 @@ void NachenBlaster::doSomething(){
                 if (getY() > 5)
                     moveTo(getX(), getY() - 6);
                 break;
-            
-            //case KEY_SPACE:
-            //case KEY_PRESS_ESCAPE:
-            //case KEY_PRESS_TAB:
+            //euclidean collisions
+            case KEY_PRESS_SPACE:
+                if (m_cabbages > 5){
+                    getWorld()->fireCabbage();
+                    m_cabbages -= 5;
+                }
+                break;
+            case KEY_PRESS_ESCAPE:
+                break;
+            case KEY_PRESS_TAB:
+                if (m_torpedoes > 0){
+                    getWorld()->fireTorpedo(true, getX() + 12, getY());
+                    m_torpedoes--;
+                }
+                break;
         }
     }
+    if (m_cabbages < 30)
+        m_cabbages++;
 }
 
 int NachenBlaster::getCabbages(){
@@ -66,10 +119,6 @@ int NachenBlaster::getCabbages(){
 
 int NachenBlaster::getTorpedoes(){
     return m_torpedoes;
-}
-
-int NachenBlaster::getHealth(){
-    return m_health * 2;
 }
 
 
@@ -81,6 +130,8 @@ Star::Star(StudentWorld* world, int x): Actor(world, IID_STAR, x, randInt(0, VIE
     //for init
 }
 
+Star::~Star(){}
+
 bool Star::isAlive(){
     if (getX() >= 0)
         return true;
@@ -90,4 +141,83 @@ bool Star::isAlive(){
 
 void Star::doSomething(){
     moveTo(getX() - 1, getY());
+}
+
+
+
+//////////////
+//Projectile//
+//////////////
+Projectile::Projectile(StudentWorld* world, int imageID, int startX, int startY, int dir): Actor(world, imageID, startX, startY, dir, 0.5, 1){}
+
+Projectile::~Projectile(){}
+
+
+
+///////////
+//Cabbage//
+///////////
+Cabbage::Cabbage(StudentWorld* world, int startX, int startY): Projectile(world, IID_CABBAGE, startX, startY){}
+
+Cabbage::~Cabbage(){}
+
+void Cabbage::doSomething(){
+    if (!isAlive())
+        return;
+    moveTo(getX() + 8, getY());
+    setDirection(getDirection() + 20);
+}
+
+bool Cabbage::isAlive(){
+    if (getX() > VIEW_WIDTH)
+        return false;
+    //also euclidian collisions
+    return true;
+}
+
+
+
+/////////////////////
+//FlatulenceTorpedo//
+/////////////////////
+FlatulenceTorpedo::FlatulenceTorpedo(StudentWorld* world, int startX, int startY, bool fromPlayer): Projectile(world, IID_TORPEDO, startX, startY, 180), m_fromPlayer(fromPlayer){
+    if (fromPlayer)
+        setDirection(0);
+}
+
+FlatulenceTorpedo::~FlatulenceTorpedo(){}
+
+bool FlatulenceTorpedo::isAlive(){
+    if (getX() < 0 || getX() > VIEW_WIDTH)
+        return false;
+    //euclid
+    return true;
+}
+
+void FlatulenceTorpedo::doSomething(){
+    if (!isAlive())
+        return;
+    if (m_fromPlayer)
+        moveTo(getX() + 8, getY());
+    else
+        moveTo(getX() - 8, getY());
+}
+
+
+
+/////////////
+//Explosion//
+/////////////
+Explosion::Explosion(StudentWorld* world, double startX, double startY):Actor(world, IID_EXPLOSION, startX, startY, 0, 1, 0){}
+
+Explosion::~Explosion(){}
+
+void Explosion::doSomething(){
+    setSize(1.5 * getSize());
+}
+
+bool Explosion::isAlive(){
+    if (getSize() > 5)
+        return false;
+    return true;
 }
