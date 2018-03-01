9c92a271daa56a55c679c1758d4bfdb151e82b99
Kennawang6 <Kenna.wang6@gmail.com>
2018-02-28 23:40:30 -0800

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -4,7 +4,10 @@
 #include "Actor.h"
 #include <list>
 #include <sstream>
+#include <cmath>
 using namespace std;
+
+
 
 GameWorld* createStudentWorld(string assetDir)
 {
@@ -15,6 +18,7 @@ GameWorld* createStudentWorld(string assetDir)
 
 StudentWorld::StudentWorld(string assetDir)
 : GameWorld(assetDir){
+    m_player = nullptr;
     //initialize new data members?
 }
 
@@ -27,6 +31,7 @@ StudentWorld::~StudentWorld(){
 int StudentWorld::init()
 {
     m_aliensLeft = 6 + getLevel() * 4;
+    m_nAliens = 0;
     m_player = new NachenBlaster(this);
     for (int i = 0; i < 30; i++)
         m_actors.push_back(new Star(this, randInt(0, VIEW_WIDTH)));
@@ -36,6 +41,8 @@ int StudentWorld::init()
 int StudentWorld::move(){
     m_player->doSomething();
     
+    introduceAlien();
+    
     //loops through all actors
     list<Actor*>::iterator obj = m_actors.begin();
     while (obj != m_actors.end()){
@@ -44,6 +51,8 @@ int StudentWorld::move(){
             (*obj)->doSomething();
         //remove dead objects
         if (!(*obj)->isAlive()){
+            if ((*obj)->isAlien())
+                m_nAliens--;
             delete (*obj);
             list<Actor*>::iterator temp = obj;
             temp--;
@@ -74,17 +83,35 @@ void StudentWorld::cleanUp(){
     }
 }
 
-void StudentWorld::fireCabbage(){
-    m_actors.push_back(new Cabbage(this, m_player->getX() + 12, m_player->getY()));
-    playSound(SOUND_PLAYER_SHOOT);
+Actor* StudentWorld::getOneCollidingAlien(const Actor* a) const{
+    list<Actor*>::const_iterator obj = m_actors.begin();
+    while (obj != m_actors.end()){
+        if((*obj)->isAlien() && euclideanDistance(a->getX(), a->getY(), (*obj)->getX(), (*obj)->getY()) < .75 * (a->getRadius() + (*obj)->getRadius()))
+            return *obj;
+        obj++;
+    }
+    return nullptr;
 }
 
-void StudentWorld::fireTorpedo(bool playerFired, int startX, int startY){
-    m_actors.push_back(new FlatulenceTorpedo(this, startX, startY, playerFired));
-    playSound(SOUND_TORPEDO);
+NachenBlaster* StudentWorld::getCollidingPlayer(const Actor* a) const{
+    if(euclideanDistance(a->getX(), a->getY(), m_player->getX(), m_player->getY()) < .75 * (a->getRadius() + m_player->getRadius()))
+        return m_player;
+    else
+        return nullptr;
 }
 
-void StudentWorld::killedAlien(){
+bool StudentWorld::playerInLineOfFire(const Actor* a) const{
+    if (m_player->getY() - a->getY() <= 4 && m_player->getY() - a->getY() >= -4)
+        return true;
+    else
+        return false;
+}
+
+void StudentWorld::addActor(Actor* a){
+    m_actors.push_back(a);
+}
+
+void StudentWorld::recordAlienDestroyed(){
     m_aliensLeft--;
 }
 
@@ -93,4 +120,25 @@ void StudentWorld::updateDisplayText(){
     stats << "Lives: " << getLives() << " Health: " << m_player->getHealth() << "% Score: " << getScore() << " Level: " << getLevel() << " Cabbages: " << m_player->getCabbages() << "% Torpedoes: " << m_player->getTorpedoes();
     string text = stats.str();
     setGameStatText(text);
+}
+
+void StudentWorld::introduceAlien(){
+    if (m_nAliens < m_aliensLeft && m_nAliens < 4 + getLevel() / 2){
+        //introducing new alien
+        int s1 = 60;
+        int s2 = 20 + getLevel() * 5;
+        int s3 = 5 + getLevel() * 10;
+        int r = randInt(1, s1 + s2 + s3);
+        if (r <= s1)
+            m_actors.push_back(new Smallgon(this, 5 * (1 + (getLevel() - 1) * .1), VIEW_WIDTH - 1, randInt(0, VIEW_HEIGHT - 1)));
+        if (r > s1 && r <= s1 + s2)
+            addActor(new Smoregon(this, 5 * (1 + (getLevel() - 1) * .1), VIEW_WIDTH - 1, randInt(0, VIEW_HEIGHT - 1)));
+        if (r > s1 + s2)
+            addActor(new Snagglegon(this, 10 * (1 + (getLevel() - 1) * .1), VIEW_WIDTH - 1, randInt(0, VIEW_HEIGHT - 1)));
+        m_nAliens++;
+    }
+}
+
+double euclideanDistance(double x1, double y1, double x2, double y2){
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
